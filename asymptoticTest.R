be1aa74954ae894fa9361710ca4a28713b99ef03
Ostrovski <vladimir_ostrovski@web.de>
2021-05-25 10:18:42 +0200

    bug fix

--- asymptoticTest.R
+++ asymptoticTest.R
@@ -7,13 +7,14 @@ asymptStDev<-function(mdr){
   p=mdr$data[[y]]
   v=p*(1-p)/w
   dd=2*mdr$residuals
-  vol=sum(dd*v*dd)/N
+  vol=sum(dd*v*dd)
   return(sqrt(vol))
 }
 
 asymptoticTest<-function(mdr){
   #calculate asymptotic min eps
-  vol = asymptStDev(mdr)
+  N=sum(mdr$weights)
+  vol = asymptStDev(mdr)/sqrt(N)
   qt=qnorm(1-mdr$alpha,0,1)
   aps = mdr$min.distance^2 + qt*vol
   return(sqrt(aps))
